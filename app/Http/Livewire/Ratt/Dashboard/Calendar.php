ee58151f0a7dc8f07be2b9858a135e322ef7db9a
gassema23 <108232923+gassema23@users.noreply.github.com>
2023-04-20 13:18:10 -0400

    change on peermissions

--- app/Http/Livewire/Ratt/Dashboard/Calendar.php
+++ app/Http/Livewire/Ratt/Dashboard/Calendar.php
@@ -15,7 +15,6 @@ class Calendar extends Component
                 $q->where('user_id', auth()->user()->id);
             })->select([DB::raw('#' || 'network_no' || ' - ' || 'network_element as title'), 'started_at as start', 'ended_at as end', 'id'])->get();
         } else {
-
             $calendar = Network::whereHas('followers', function ($q) {
                 $q->where('user_id', auth()->user()->id);
             })->select([DB::raw('#' || 'network_no' || ' - ' || 'network_element as title'), 'started_at as start', 'ended_at as end', 'id'])->get();
