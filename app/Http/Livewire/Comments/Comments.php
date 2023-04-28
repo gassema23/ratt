297e41ce859c5a2aa28db6018499f9d43649a2ce
gassema23 <108232923+gassema23@users.noreply.github.com>
2023-04-28 09:53:09 -0400

    comments update

--- app/Http/Livewire/Comments/Comments.php
+++ app/Http/Livewire/Comments/Comments.php
@@ -28,31 +28,28 @@ class Comments extends Component
 
     public function postComment()
     {
-
         $this->validate([
             'newCommentState.body' => 'required'
         ]);
-
         $comment = $this->model->comments()->make($this->newCommentState);
         $comment->user()->associate(auth()->user());
         $comment->save();
-
-        if($this->model instanceof NetworkTask){
-        $this->model->project->planner->notify(new DatabaseNotification(
-            $type = 'info',
-            $message = auth()->user()->name,
-            $messageLong =  trans(' Add a new comment'),
-            $href = '/admin/networks/show/'.$this->model->id,
-            $hrefText = trans('View')
-        ));
-        $this->model->project->prime->notify(new DatabaseNotification(
-            $type = 'info',
-            $message = auth()->user()->name,
-            $messageLong =  trans(' Add a new comment'),
-            $href = '/admin/networks/show/'.$this->model->id,
-            $hrefText = trans('View')
-        ));
-}
+        if ($this->model instanceof NetworkTask) {
+            $this->model->project->planner->notify(new DatabaseNotification(
+                $type = 'info',
+                $message = auth()->user()->name,
+                $messageLong =  trans(' Add a new comment'),
+                $href = '/admin/networks/show/' . $this->model->id,
+                $hrefText = trans('View')
+            ));
+            $this->model->project->prime->notify(new DatabaseNotification(
+                $type = 'info',
+                $message = auth()->user()->name,
+                $messageLong =  trans(' Add a new comment'),
+                $href = '/admin/networks/show/' . $this->model->id,
+                $hrefText = trans('View')
+            ));
+        }
         $this->newCommentState = [
             'body' => ''
         ];
--- resources/views/livewire/comments/comments.blade.php
+++ resources/views/livewire/comments/comments.blade.php
@@ -1,38 +1,35 @@
 <section>
-    <div class="">
-        <div class="divide-y divide-slate-200">
-            <div class="pt-4">
-                <div class="space-y-8">
-                    @if ($comments->isNotEmpty())
-                        @foreach ($comments as $comment)
-                            <livewire:comments.comment :comment="$comment" :key="$comment->id" />
-                        @endforeach
-                        {{ $comments->links() }}
-                    @else
-                        <p class="inline-flex items-center gap-x-2 py-1.5 font-medium text-slate-400">
-                            @lang('No comments yet.')
-                        </p>
-                    @endif
-                </div>
+    <div class="divide-y divide-slate-200">
+        <div class="pt-4">
+            <div class="space-y-8">
+                @if ($comments->isNotEmpty())
+                    @foreach ($comments as $comment)
+                        <livewire:comments.comment :comment="$comment" :key="$comment->id" />
+                    @endforeach
+                    {{ $comments->links() }}
+                @else
+                    <p class="inline-flex items-center gap-x-2 py-1.5 font-medium text-slate-400">
+                        @lang('No comments yet.')
+                    </p>
+                @endif
             </div>
         </div>
-        <div class="bg-slate-50 p-4">
-            <div class="flex">
-                <div class="flex-shrink-0 mr-4">
-                    <img class="h-10 w-10 rounded-full" src="{{ auth()->user()->gravatar }}"
-                        alt="{{ auth()->user()->name }}">
-                </div>
-                <div class="min-w-0 flex-1">
-                    <form wire:submit.prevent="postComment">
-                        <div>
-                            <x-textarea wire:model.defer="newCommentState.body" id="comment" :label="trans('Comment')"
-                                :placeholder="trans('Your comment')" />
-                        </div>
-                        <div class="mt-3 flex items-center justify-end">
-                            <x-button xs squared positive wire:click="postComment" :label="trans('Comment')" spinner />
-                        </div>
-                    </form>
-                </div>
+    </div>
+    <div class="bg-slate-50 p-4">
+        <div class="flex">
+            <div class="flex-shrink-0 mr-4">
+                <img class="h-10 w-10 rounded-full" src="{{ auth()->user()->gravatar }}" alt="{{ auth()->user()->name }}">
+            </div>
+            <div class="min-w-0 flex-1">
+                <form wire:submit.prevent="postComment">
+                    <div>
+                        <x-textarea wire:model.defer="newCommentState.body" id="comment" :label="trans('Comment')"
+                            :placeholder="trans('Your comment')" />
+                    </div>
+                    <div class="mt-3 flex items-center justify-end">
+                        <x-button xs squared positive wire:click="postComment" :label="trans('Comment')" spinner />
+                    </div>
+                </form>
             </div>
         </div>
     </div>
