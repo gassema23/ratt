85f07fd6d7dba877c9457e8f63f84e9da8746942
gassema23 <108232923+gassema23@users.noreply.github.com>
2023-04-28 08:58:47 -0400

    create network

--- resources/views/livewire/ratt/networks/create.blade.php
+++ resources/views/livewire/ratt/networks/create.blade.php
@@ -20,7 +20,7 @@
                     <x-datetime-picker :label="__('Ended date')" wire:model.defer="ended_at" without-time :hint="trans('Field required')" />
                 </div>
                 <div class="grid grid-cols-2 gap-2 my-2">
-                    <x-input wire:model.defer="name.en" :label="__('Name')" :hint="trans('Field required')" />
+                    <x-input wire:model.defer="name" :label="__('Name')" :hint="trans('Field required')" />
                 </div>
                 <div class="grid grid-cols-1 gap-2 my-2">
                     <livewire:trix>
