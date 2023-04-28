c624d6fea77d847a08826291b59214ead847e18e
gassema23 <108232923+gassema23@users.noreply.github.com>
2023-04-28 09:40:17 -0400

    comments permissions

--- resources/views/livewire/comments/comments.blade.php
+++ resources/views/livewire/comments/comments.blade.php
@@ -16,26 +16,24 @@
                 </div>
             </div>
         </div>
-        @can('comments-create')
-            <div class="bg-slate-50 p-4">
-                <div class="flex">
-                    <div class="flex-shrink-0 mr-4">
-                        <img class="h-10 w-10 rounded-full" src="{{ auth()->user()->gravatar }}"
-                            alt="{{ auth()->user()->name }}">
-                    </div>
-                    <div class="min-w-0 flex-1">
-                        <form wire:submit.prevent="postComment">
-                            <div>
-                                <x-textarea wire:model.defer="newCommentState.body" id="comment" :label="trans('Comment')"
-                                    :placeholder="trans('Your comment')" />
-                            </div>
-                            <div class="mt-3 flex items-center justify-end">
-                                <x-button xs squared positive wire:click="postComment" :label="trans('Comment')" spinner />
-                            </div>
-                        </form>
-                    </div>
+        <div class="bg-slate-50 p-4">
+            <div class="flex">
+                <div class="flex-shrink-0 mr-4">
+                    <img class="h-10 w-10 rounded-full" src="{{ auth()->user()->gravatar }}"
+                        alt="{{ auth()->user()->name }}">
+                </div>
+                <div class="min-w-0 flex-1">
+                    <form wire:submit.prevent="postComment">
+                        <div>
+                            <x-textarea wire:model.defer="newCommentState.body" id="comment" :label="trans('Comment')"
+                                :placeholder="trans('Your comment')" />
+                        </div>
+                        <div class="mt-3 flex items-center justify-end">
+                            <x-button xs squared positive wire:click="postComment" :label="trans('Comment')" spinner />
+                        </div>
+                    </form>
                 </div>
             </div>
-        @endcan
+        </div>
     </div>
 </section>
