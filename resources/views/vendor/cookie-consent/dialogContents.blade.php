9887599a153c8c37e7a9d5a7cd4bfea8166f91c1
gassema23 <108232923+gassema23@users.noreply.github.com>
2023-05-02 10:09:32 -0400

    update query on todo list

--- resources/views/vendor/cookie-consent/dialogContents.blade.php
+++ resources/views/vendor/cookie-consent/dialogContents.blade.php
@@ -1,14 +1,13 @@
-<div class="js-cookie-consent cookie-consent pb-2 z-30">
-    <div class="p-4 border rounded-lg shadow-soft bg-yellow-50">
-        <div class="flex items-center justify-between flex-wrap">
-            <div class="w-0 flex-1 items-center hidden lg:inline">
-                <p class="ml-3 text-slate-700 cookie-consent__message">
-                    {!! trans('cookie-consent::texts.message') !!}
-                </p>
-            </div>
-            <div class="mt-2 flex-shrink-0 w-full sm:mt-0 sm:w-auto">
-                <x-button warning squared :label="trans('cookie-consent::texts.agree')" class="js-cookie-consent-agree cookie-consent__agree" />
-            </div>
+<div
+    class="js-cookie-consent cookie-consent rounded-lg bg-warning-50 dark:bg-secondary-800 dark:border dark:border-warning-600 p-4">
+    <div class="flex items-center justify-between align-middle">
+        <div>
+            <span class="text-sm font-semibold text-warning-800 dark:text-warning-600 cookie-consent__message">
+                {!! trans('cookie-consent::texts.message') !!}
+            </span>
+        </div>
+        <div>
+            <x-button outline warning squared :label="trans('cookie-consent::texts.agree')" class="js-cookie-consent-agree cookie-consent__agree" />
         </div>
     </div>
 </div>
