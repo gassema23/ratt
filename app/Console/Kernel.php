ae9b7aa4d41b21318255a89884b3c6ce0b1e3f47
gassema23 <108232923+gassema23@users.noreply.github.com>
2023-06-07 09:40:09 -0400

    database backup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,42 +10,14 @@ class Kernel extends ConsoleKernel
 
     protected $commands = [
         \App\Console\Commands\MySqlDump::class,
-        \App\Console\Commands\IsqImportCsvFile::class,
-        \App\Console\Commands\MilestoneImportCsvFile::class,
-        \App\Console\Commands\BiriNetworkPlan::class,
+        \App\Console\Commands\DatabaseBackup::class,
     ];
     /**
      * Define the application's command schedule.
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
-        $schedule->command('backup:clean')->daily()->at('01:00');
-        $schedule->command('backup:run')->daily()->at('01:30');
-
-        $schedule->command('csv:import-ISQ003')
-            ->daily()
-            ->at('01:30')
-            ->runInBackground()
-            ->evenInMaintenanceMode()
-            ->emailOutputTo('mathieu.gasse2@telus.com')
-            ->emailOutputOnFailure('mathieu.gasse2@telus.com');
-
-        $schedule->command('csv:import-PS44B')
-            ->daily()
-            ->at('01:30')
-            ->runInBackground()
-            ->evenInMaintenanceMode()
-            ->emailOutputTo('mathieu.gasse2@telus.com')
-            ->emailOutputOnFailure('mathieu.gasse2@telus.com');
-
-        $schedule->command('csv:import-PS50')
-            ->daily()
-            ->at('01:30')
-            ->runInBackground()
-            ->evenInMaintenanceMode()
-            ->emailOutputTo('mathieu.gasse2@telus.com')
-            ->emailOutputOnFailure('mathieu.gasse2@telus.com');
+        $schedule->command('database:backup')->daily();
     }
 
     /**
